646665694d508467050d605155f7031715f2a398
Hazhir Derakhshi <hajear@gmail.com>
2026-03-06 12:35:12 +0000

    Setup lsp and formatter for Python

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,46 @@
 require("nvchad.configs.lspconfig").defaults()
 
+local function on_attach(_, _)
+  vim.diagnostic.config {
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = { source = true, border = "rounded" },
+  }
+end
+
 local servers = {
   "html",
   "cssls",
   "pyright",
+  "ruff",
 }
+
+vim.lsp.config("pyright", {
+  on_attach = on_attach,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "workplace",
+      },
+    },
+  },
+})
+
+vim.lsp.config("ruff", {
+  on_attach = on_attach,
+  init_options = {
+    settings = {
+      args = {},
+    },
+  },
+})
+
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
--- lua/options.lua
+++ lua/options.lua
@@ -1,22 +1,23 @@
 require "nvchad.options"
 
 local opt = vim.opt
-local opt_local = vim.opt_local
 local o = vim.o
 -- local g = vim.g
 
 -------------------------------------- options ------------------------------------------
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 
 opt.foldlevel = 99
 opt.foldlevelstart = 99
 opt.foldenable = true
-opt.foldmethod = 'expr'
-opt.foldexpr = 'nvim_treesitter#foldexpr()'
-opt.indentexpr = 'nvim_treesitter#indent()'
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.indentexpr = "nvim_treesitter#indent()"
 opt.scrolloff = 10
 
-opt_local.colorcolumn = '80,100,120'
-vim.api.nvim_set_hl(0, 'ColorColumn', {
-  bg = '#73c9bb',
-})
+opt.colorcolumn = "80,100,120"
+-- vim.api.nvim_set_hl(0, "ColorColumn", {
+--   bg = "#21200b",
+-- })
+
+vim.o.updatetime = 250 -- need this for CursorHold autocmd in autocmds.lua
